a48ffa74c5393152fbdbca89a78ad368e259eaa7
gustavosalgadocba <agustinbritoscba1984@gmail.com>
2024-07-09 10:02:18 -0300

    actu

--- frontend/Js/clase.js
+++ frontend/Js/clase.js
@@ -1,11 +1,3 @@
-// class Productos{
-//     constructor(nombre,características,precio){
-//         this.nombre = nombre;
-//         this.características = características;
-//         this.precio = precio;
-//     }
-// }
-
 class Producto {
     constructor(nombre, caracteristicas, precio) {
         this.nombre = nombre;
